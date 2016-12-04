4aaa3a47862302c986e50afb858bf6f1720a7e2b
cgulli <caitlin.gulliford@gmail.com>
2016-12-04 15:44:49 +0000

    added test to initialize the graph class with an empty nodes array

--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -1,5 +1,11 @@
-# require 'graph'
-#
-# describe Graph do
-#
-# end
+require 'graph'
+
+describe Graph do
+
+  describe '#initialize'do
+  graph = Graph.new
+    it 'stores a collection of nodes/stations' do
+      expect(graph.nodes).to be_empty
+    end
+  end
+end
