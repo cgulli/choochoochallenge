26938ccd89477d5ebfb2c82eda0a6f52485095f3
cgulli <caitlin.gulliford@gmail.com>
2016-12-05 18:25:54 +0000

    stuck as to how to continue testing methods for planner class

--- spec/planner_spec.rb
+++ spec/planner_spec.rb
@@ -1,9 +1,8 @@
-# require_relative 'graph'
-#
-# describe Planner do
-#   context '#initialize' do
-#     it 'creates a graph with input data'
-# 
-# # a = Planner.new("AB5\nCB7\nAD8")
-#   end
-# end
+require 'planner'
+
+describe Planner do
+  context '#initialize' do
+    it 'creates a graph with input data'
+    planner = Planner.new("AB5\nCB7\nAD8")
+  end
+end
