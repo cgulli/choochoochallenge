7b7ec2cfe6acc62c316033069a749e2809e4c3dc
cgulli <caitlin.gulliford@gmail.com>
2016-12-05 18:02:37 +0000

    encountered ruby gem error so updated graph spec tests temporarily

--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -3,9 +3,9 @@ require 'node'
 require 'edge'
 
 describe Graph do
-subject(:graph) {described_class.new}
-
+# subject(:graph) {described_class.new}
   context '#initialize'do
+  graph = Graph.new
     it 'stores a collection of nodes/stations' do
       graph.create_node('A')
       expect(graph.nodes[0].name).to eq 'A'
@@ -13,9 +13,10 @@ subject(:graph) {described_class.new}
   end
 
   context '#creates routes' do
+    graph = Graph.new
+    graph.create_node('A')
     it 'adds nodes to array' do
-      expect(graph).to respond_to(:create_node).with(1).argument
-      # change length of node array
+      expect(graph.node_count).to eq (1)
     end
     it 'adds an edge' do
       expect(graph).to respond_to(:add_edge).with(3).arguments
@@ -24,6 +25,7 @@ subject(:graph) {described_class.new}
   end
 
   context '#manages nodes' do
+    graph = Graph.new
     graph.create_node('A')
     graph.create_node('B')
     it 'finds nodes by name' do
